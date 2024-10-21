fa78b5a85cdc876290dca7c20e65088c826d0dc9
Nunez686 <nunezbrayan443@gmail.com>
2024-10-21 09:32:03 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,14 @@
+<?php
+/* CBTIS 89 
+P34Array15.php
+   Programa que
+   Brayan Nuñez
+   3ºA Programaciòn Matutino
+   */
+
+   echo "Mi nombre es Brayan Nuñez";
+   echo "<br>", "Estudio en el CBTIS89 ";
+   echo "<br>", "En tercer semestre de la carrera de programacion A Matutino";
+   echo "<br>", "Vivo en la cuidad de Durango,Dgo.";
+
+   ?>
