cb906b432d8412c2653041194262868ef66a6ef2
Nunez686 <nunezbrayan443@gmail.com>
2024-11-25 08:58:41 -0600

    Update P38Array18.php

--- Unidad3NuñezSoto/P38Array18.php
+++ Unidad3NuñezSoto/P38Array18.php
@@ -12,7 +12,7 @@
 echo "<br>";
 echo "NORMAL";
 echo "<br>";
-// Imprimir la matriz en su forma original
+
 for ($i = 0; $i < 3; $i++) {
     for ($j = 0; $j < 3; $j++) {
         echo $datos[$i][$j] . " ";
@@ -23,7 +23,7 @@ echo "<br>";
 
 echo "FILAS A COLUMNAS";
 echo "<br>";
-// Imprimir la matriz transpuesta (filas a columnas)
+
 for ($i = 0; $i < 3; $i++) {
     for ($j = 0; $j < 3; $j++) {
         echo $datos[$j][$i] . " ";
@@ -31,10 +31,9 @@ for ($i = 0; $i < 3; $i++) {
     echo "<br>";
 }
 echo "<br>";
-
 echo "COLUMNAS INVERTIDAS";
 echo "<br>";
-// Imprimir las columnas invertidas (cada fila con las columnas al revés)
+
 for ($i = 0; $i < 3; $i++) {
     for ($j = 2; $j >= 0; $j--) { 
         echo $datos[$i][$j] . " ";
@@ -42,11 +41,9 @@ for ($i = 0; $i < 3; $i++) {
     echo "<br>";
 }
 echo "<br>";
-
 echo "FILAS INVERTIDAS";
 echo "<br>";
-// Imprimir las filas invertidas (la última fila primero)
-for ($i = 2; $i >= 0; $i--) { // Comenzar desde la última fila
+for ($i = 2; $i >= 0; $i--) { 
     for ($j = 0; $j < 3; $j++) {
         echo $datos[$i][$j] . " ";
     }
